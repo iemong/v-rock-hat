e3d11b7e98db77cd33cc959e094a789578c91717
iemong <irie.s@note.jp>
2023-12-15 23:52:23 +0900

    グレーの帽子被ってるか判定できる

--- src/app/input/VideoInput.tsx
+++ src/app/input/VideoInput.tsx
@@ -1,44 +1,100 @@
-'use client'
-import {useCallback, useRef} from "react";
+"use client";
+
+import { useCallback, useRef, useTransition } from "react";
+import { analyzeImageIfWearRockCap } from "@/app/input/actions";
 
 export const VideoInput = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const streamRef = useRef<MediaStream | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-    const videoRef = useRef<HTMLVideoElement | null>(null);
-    const streamRef = useRef<MediaStream | null>(null);
+  const [isPending, startTransition] = useTransition();
 
-    const playVideo = useCallback(async () => {
-        reset()
-        const { VirtualBackground } = await import('skyway-video-processors');
-        const backgroundProcessor = new VirtualBackground({ image: 'green.png' });
-        await backgroundProcessor.initialize();
-        const result = await backgroundProcessor.createProcessedStream();
-        if (result === null || result.track === null) {
-            return;
-        }
-        const mediaStream = new MediaStream([result.track]);
-        streamRef.current = mediaStream;
-        if (!videoRef.current) {
-            return;
-        }
-        videoRef.current.srcObject = mediaStream;
-        videoRef.current.onloadedmetadata = () => {
-            videoRef.current?.play()
-        }
-    }, [])
+  const playVideo = useCallback(async () => {
+    reset();
+    const { VirtualBackground } = await import("skyway-video-processors");
+    const backgroundProcessor = new VirtualBackground({ image: "green.png" });
+    await backgroundProcessor.initialize();
+    const result = await backgroundProcessor.createProcessedStream();
+    if (result === null || result.track === null) {
+      return;
+    }
+    const mediaStream = new MediaStream([result.track]);
+    streamRef.current = mediaStream;
+    if (!videoRef.current) {
+      return;
+    }
+    videoRef.current.srcObject = mediaStream;
+    videoRef.current.onloadedmetadata = () => {
+      videoRef.current?.play();
+    };
+  }, []);
 
-    const reset = () => {
-        streamRef.current?.getTracks().forEach(track => track.stop());
-        streamRef.current = null;
-        if (videoRef.current) {
-            videoRef.current?.pause()
-            videoRef.current.srcObject = null;
-        }
+  const reset = () => {
+    streamRef.current?.getTracks().forEach((track) => track.stop());
+    streamRef.current = null;
+    if (videoRef.current) {
+      videoRef.current?.pause();
+      videoRef.current.srcObject = null;
+    }
+  };
+
+  const captureImage = useCallback(() => {
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
+    if (video === null || canvas === null) {
+      return;
+    }
+    const ctx = canvas.getContext("2d");
+    if (ctx === null) {
+      return;
     }
 
-    return (
-        <div>
-            <button onClick={playVideo}>再生</button>
-            <video ref={videoRef} autoPlay playsInline/>
-        </div>
-    )
-}
+    const size = Math.min(video.videoWidth, video.videoHeight);
+
+    const startX = (video.videoWidth - size) / 2;
+    const startY = (video.videoHeight - size) / 2;
+
+    const canvasSize = 512;
+    canvas.width = canvasSize;
+    canvas.height = canvasSize;
+
+    ctx.drawImage(
+      video,
+      startX,
+      startY,
+      size,
+      size,
+      0,
+      0,
+      canvasSize,
+      canvasSize,
+    );
+    return canvas.toDataURL("image/jpeg");
+  }, []);
+
+  const handleAnalyze = () => {
+    const base64Image = captureImage();
+    if (base64Image === undefined) {
+      return;
+    }
+    startTransition(async () => {
+      const res = await analyzeImageIfWearRockCap(base64Image);
+      console.log(res);
+    });
+  };
+  return (
+    <div>
+      <button onClick={playVideo}>再生</button>
+      <button onClick={captureImage}>スクショ</button>
+      <button onClick={handleAnalyze}>分析</button>
+      <video
+        ref={videoRef}
+        autoPlay
+        playsInline
+        className={"w-screen h-screen object-cover object-center"}
+      />
+      <canvas width={512} height={512} ref={canvasRef} />
+    </div>
+  );
+};
---
+++ src/app/input/actions.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import OpenAI from "openai";
+import { env } from "@/env/server.mjs";
+
+export async function analyzeImageIfWearRockCap(base64String: string) {
+  const openai = new OpenAI({
+    apiKey: env.OPENAI_API_KEY,
+  });
+  const res = await openai.chat.completions.create({
+    model: "gpt-4-vision-preview",
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: "If you see someone wearing a gray hat in the picture, return it as true. Otherwise, return false.",
+          },
+          {
+            type: "image_url",
+            image_url: {
+              url: base64String,
+              detail: "low",
+            },
+          },
+        ],
+      },
+    ],
+  });
+  const result = res.choices[0];
+  return result.message.content?.toLowerCase() === "true";
+}
